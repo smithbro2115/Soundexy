bb8293988ff3d60c7a59a5c5150dc88c202f1e2f
Josh <smithbro2115@gmail.com>
2018-12-13 17:31:30 -0600

    Still working on the audio player

--- AudioPlayer.py
+++ AudioPlayer.py
@@ -112,7 +112,6 @@ class SoundPlayer(QRunnable):
 
     @pyqtSlot()
     def run(self):
-        pass
         while True:
             while self.is_playing and not self.ended:
                 # start_time = time.time()
