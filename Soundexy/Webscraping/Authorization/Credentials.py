038a0374e4033e549dfcbab64bf1bb24382b2ea8
Josh <smithbro2115@gmail.com>
2019-05-20 18:04:19 -0500

    - Added basic encryption for storing passwords

--- Soundexy/Webscraping/Authorization/Credentials.py
+++ Soundexy/Webscraping/Authorization/Credentials.py
@@ -1,9 +1,7 @@
 import configparser
 from Soundexy.Functionality.useful_utils import get_app_data_folder
 from Soundexy.GUI.API.CustomPyQtWidgets import LoginDialog
-
-
-# TODO encrypt credentials before saving them to ini file
+from cryptography.fernet import Fernet
 
 
 downloader_config_path = f"{get_app_data_folder('obj')}/downloader_auth.ini"
@@ -20,14 +18,14 @@ def delete_saved_credentials(site_name):
 def get_saved_credentials(site_name):
     config = configparser.ConfigParser()
     config.read(downloader_config_path)
-    return config[site_name]['user'], config[site_name]['password']
+    return config[site_name]['user'], decrypt(config[site_name]['password'])
 
 
 def save_credentials(user, password, site):
     config = configparser.ConfigParser()
     config[site] = {}
     config[site]['User'] = user
-    config[site]['Password'] = password
+    config[site]['Password'] = encrypt(password)
     with open(downloader_config_path, 'w+') as config_file:
         config.write(config_file)
 
@@ -42,3 +40,17 @@ def get_credentials(website):
         if dialog.ui.rememberCheckBox.checkState():
             save_credentials(dialog.ui.userLineEdit.text(), dialog.ui.passwordLineEdit.text(), website)
     return credentials
+
+
+def encrypt(secure_data):
+    byte_text = secure_data.encode()
+    secret = b'RMi8CkGD8ER3uJvEWCF6hIivU3hrV2TtiTY3vnIJiLg='
+    fernet = Fernet(secret)
+    return fernet.encrypt(byte_text).decode()
+
+
+def decrypt(encrypted_data):
+    byte_text = encrypted_data.encode()
+    secret = b'RMi8CkGD8ER3uJvEWCF6hIivU3hrV2TtiTY3vnIJiLg='
+    fernet = Fernet(secret)
+    return fernet.decrypt(byte_text).decode()
