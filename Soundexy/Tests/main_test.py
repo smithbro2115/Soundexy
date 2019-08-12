81d963fe1295ec00c4d83ef5fc74c792ecdd03a0
Josh <smithbro2115@gmail.com>
2019-08-12 17:53:55 -0500

    - Working on soundogs test

--- Soundexy/Tests/main_test.py
+++ Soundexy/Tests/main_test.py
@@ -1,25 +1,14 @@
-from bs4 import BeautifulSoup, SoupStrainer
+from bs4 import BeautifulSoup
 from Soundexy.Webscraping.Webscraping.WebScraperRequester import simple_get
+import requests
 from lxml import html
 import time
 
 
-def get_results(raw_html):
-	try:
-		print('first')
-		strainer = SoupStrainer('table', attrs={'id': 'searchResultsTable'})
-		html = BeautifulSoup(raw_html, 'lxml', parse_only=strainer)
-		table = html.find_all('table', {'id': 'searchResultsTable'})
-		return table[0].find('tbody').find_all('tr')
-	except Exception as e:
-		print("Something didn't work!  " + str(e))
+register_url = "http://api.sounddogs.com/api/v1/licensing/register"
+post_data = {"reqType": 1, 'email': 'brinkmansound@gmail.com', 'licVer': '4699', 'appVer': '1.3.1'}
 
+r = requests.post(register_url, json=post_data)
+token = r.json()['token']
 
-url = "https://www.sounddogs.com/search?keywords=humvee"
-raw_html = simple_get(url)
-print('lxml start')
-start = time.time()
-processed = html.fromstring(raw_html)
-table = processed.xpath('//table[@id="searchResultsTable"]')
-print(html.tostring(table[0]))
-print('lxml stop, total time: ' + str(time.time() - start))
+print(token)
