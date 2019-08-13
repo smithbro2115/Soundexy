aa34ad1bf0701536850985f8843e3dc44930e923
Joshua <smithbro2115@gmail.com>
2019-08-13 12:51:31 -0500

    - Did some more tests with Sound Dogs API

--- Soundexy/Tests/main_test.py
+++ Soundexy/Tests/main_test.py
@@ -6,9 +6,13 @@ import time
 
 
 register_url = "http://api.sounddogs.com/api/v1/licensing/register"
-post_data = {"reqType": 1, 'email': 'brinkmansound@gmail.com', 'licVer': '4699', 'appVer': '1.3.1'}
+register_post_data = {"reqType": 1, 'email': 'brinkmansound@gmail.com', 'licVer': '4699', 'appVer': '1.3.1'}
 
-r = requests.post(register_url, json=post_data)
+r = requests.post(register_url, json=register_post_data)
 token = r.json()['token']
 
-print(token)
+search_url = "http://api.sounddogs.com/api/v1/audioclips"
+search_post_data = {'keyword': 'tree', 'pageNumber': 2}
+search_headers = {'X-Auth-Token': token}
+search_r = requests.post(search_url, json=search_post_data, headers=search_headers)
+print(search_r, search_r.json())
